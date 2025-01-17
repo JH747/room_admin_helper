77ddb108550bbf3da505a976fc4deb320d06fe97
Junhyung Oh <90770637+JH747@users.noreply.github.com>
2025-01-18 00:46:21 +0900

    [front] changed retrieve analysis page to use websocket

--- frontend/src/app/analysis/retrieve/page.js
+++ frontend/src/app/analysis/retrieve/page.js
@@ -8,50 +8,108 @@ import DatePicker from 'react-datepicker';
 import dayjs from 'dayjs';
 
 import SalesChart from './saleschart';
+import { Client } from '@stomp/stompjs';
+import { useEffect } from 'react';
 
 export default function Page() {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [data, setData] = useState(null);
   const [isChartVisible, setIsChartVisible] = useState(false); // 차트 표시 여부
+  const [client, setClient] = useState(null);
+
+  useEffect(() => {
+    const connectWebSocket = async () => {
+      try {
+        const session = await getSession(); // 세션 비동기 호출
+        if (!session || !session.token) {
+          console.error('Session or token is missing.');
+          return;
+        }
+
+        const stompClient = new Client({
+          brokerURL: 'ws://localhost:8080/ws',
+          connectHeaders: {
+            Authorization: `Bearer ${session.token}`, // 토큰 추가
+          },
+          debug: function (str) {
+            console.log(str);
+          },
+          reconnectDelay: 5000,
+          heartbeatIncoming: 4000,
+          heartbeatOutgoing: 4000,
+        });
+
+        stompClient.onConnect = function (frame) {
+          console.log('Connected: ' + frame);
+          stompClient.subscribe('/queue/general', function (message) {
+            console.log('From Server: ' + message.body);
+            const data = JSON.parse(message.body);
+            setData(data);
+            setIsChartVisible(true); // 데이터가 설정되면 차트를 표시
+          });
+        };
+
+        stompClient.activate();
+        setClient(stompClient);
+      } catch (error) {
+        console.error('Error connecting to WebSocket:', error);
+      }
+    };
+
+    connectWebSocket();
+
+    return () => {
+      if (client) {
+        client.deactivate(); // 컴포넌트 언마운트 시 WebSocket 연결 해제
+      }
+    };
+  }, []);
 
   async function handleSubmit() {
     const sDate = dayjs(startDate).format('YYYY-MM-DD');
     const eDate = dayjs(endDate).format('YYYY-MM-DD');
-    const session = await getSession();
 
-    const EventSource = EventSourcePolyfill;
+    if (client) {
+      client.publish({
+        destination: '/app/analysis',
+        body: JSON.stringify({
+          start_date: sDate,
+          end_date: eDate,
+          mode: 'retrieve',
+        }),
+      });
+    }
 
-    // console.log(session.token);
-
-    const eventSource = new EventSource(
-      `/api/proxy_retrieve_info?start_date=${sDate}&end_date=${eDate}&mode=retrieve`,
-      {
-        headers: {
-          Authorization: `Bearer ${session.token}`,
-        },
-      }
-    );
-    eventSource.onmessage = (event) => {
-      try {
-        const data = JSON.parse(event.data);
-        if (data.status == 'Ping') {
-          console.log('Ping received');
-        } else if (data.status == 'Connection opened') {
-          console.log('Connection opened');
-        } else {
-          setData(data);
-          setIsChartVisible(true); // 데이터가 설정되면 차트를 표시
-          eventSource.close();
-        }
-      } catch (error) {
-        console.error('JSON parsing error:', error);
-      }
-    };
-    eventSource.onerror = (error) => {
-      console.error('SSE error:', error);
-      eventSource.close();
-    };
+    // const EventSource = EventSourcePolyfill;
+    // const eventSource = new EventSource(
+    //   `/api/proxy_retrieve_info?start_date=${sDate}&end_date=${eDate}&mode=retrieve`,
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${session.token}`,
+    //     },
+    //   }
+    // );
+    // eventSource.onmessage = (event) => {
+    //   try {
+    //     const data = JSON.parse(event.data);
+    //     if (data.status == 'Ping') {
+    //       console.log('Ping received');
+    //     } else if (data.status == 'Connection opened') {
+    //       console.log('Connection opened');
+    //     } else {
+    //       setData(data);
+    //       setIsChartVisible(true); // 데이터가 설정되면 차트를 표시
+    //       eventSource.close();
+    //     }
+    //   } catch (error) {
+    //     console.error('JSON parsing error:', error);
+    //   }
+    // };
+    // eventSource.onerror = (error) => {
+    //   console.error('SSE error:', error);
+    //   eventSource.close();
+    // };
   }
 
   return (
